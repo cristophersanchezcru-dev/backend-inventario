1f96ab3faf0061202f72b601ee1945f91c0dc130
Cris <cristophersanchezcru@gmail.com>
2026-07-27 11:52:01 -0600

    primer commit

---
+++ producto/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class producto(models.Model):
+    nombre=models.CharField(max_length=100)
+    precio=models.FloatField()
+    descripcion=models.TextField()
+    created=models.DateField(auto_now_add=True)
+    updated=models.DateField(auto_now=True)
+    def __str__(self):
+     return self.nombre
